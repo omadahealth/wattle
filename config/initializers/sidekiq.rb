d8f51ca1237f53df9ad259fe4860fe6c6f623e0c
Kevin G. Phillips <kevin.phillips@omadahealth.com>
2016-10-31 16:37:39 -0700

    Remove sidekiq service registration in initializer in favor of after_initialize

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,7 +3,3 @@ schedule_file = "#{Rails.root}/config/sidekiq_schedule.yml"
 if File.exists?(schedule_file) && Sidekiq.server?
   Sidekiq::Cron::Job.load_from_hash YAML.load(ERB.new(File.read(schedule_file)).result)
 end
-
-service = Apohypaton::Service.new()
-service.service_type = 'sidekiq'
-service.register()
